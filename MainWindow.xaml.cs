704565fc5ba52ede7f03d68e80b08b8d45e3febe
DiMadagascar <missmadagascar@mail.ru>
2022-01-01 15:32:55 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace HW_10
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        TelegramMessageClient client;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            client = new TelegramMessageClient(this);
+
+            logList.ItemsSource = client.BotMessageLog;
+        }
+
+        private void btnMsgSendClick(object sender, RoutedEventArgs e)
+        {
+            client.SendMessage(txtMsgSend.Text, TargetSend.Text);
+        }
+        private void saveAsJsonClick(object sender, RoutedEventArgs e)
+        {
+            TelegramMessageClient.JsonSerializer(client.BotMessageLog);
+        }
+    }
+}
